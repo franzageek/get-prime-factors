7b9ed648b050f2f23a8eaccab88771d2d13a52ce
franzageek <88248950+franzageek@users.noreply.github.com>
2024-07-12 23:45:53 +0200

    Update core.h according to core.c’s last commit

--- core.h
+++ core.h
@@ -6,7 +6,7 @@
 
 extern const size_t primes_size;
 extern const size_t queue_size;
-bool is_prime(long long number, int* primes);
+bool is_prime(long long number);
 int* init_primes(void);
 int* init_queue(void);
 
