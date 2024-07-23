016786b6447db634a6d8f9e922afc2d84e8cacf7
franzageek <88248950+franzageek@users.noreply.github.com>
2024-07-23 16:37:17 +0200

    Update main.c to make it use libprimes

--- main.c
+++ main.c
@@ -1,48 +1,44 @@
-#include "core.h"
+#include <primes.h>
+#include <string.h>
+#define queue_size 100
 
 int main(int argc, char const *argv[])
 {
     if (argc > 1)
-    {   long long input = strtoll(argv[1],  NULL, 10);
-        int* primes = (int*)init_primes();
-        
-        if (is_prime(input)) //Check if input is in the correct format
+    {   
+        u64 total = strtoull(argv[1], NULL, 10);
+        long long input = strtoll(argv[1],  NULL, 10);
+        if (input <= 0) //Check if input is in the correct format
         {
-            fprintf(stderr, "Number is prime (-1)\n");
-            free(primes); 
-            exit(-1);
+            fprintf(stderr, "Number is 0, negative or in the wrong format (-1)\n");
+            exit(-1); 
         }
-        if (input <= 0) 
+        if (total == 0xFFFFFFFFFFFFFFFF && (strcmp(argv[1], "18446744073709551615") != 0))
         {
-            fprintf(stderr, "Number is 0, negative or in the wrong format (-2)\n");
-            free(primes); 
-            exit(-2); 
+            fprintf(stderr, "Number is too big (-2)\n");
+            exit(-2);
         }
-        if (input == 1)
+        if (total == 1)
         {
             fprintf(stderr, "Number is 1 (-3)\n");
-            free(primes);
             exit(-3);
         }
-        int* queue = (int*)init_queue();
-        int counter = 0;
-        unsigned long long result = input;
-        for(int i = 0; i<primes_size; ++i) //Stack every prime number that is divisible for the result of the last division;
+        if (is_prime(total)) 
         {
-            if (result == 1)
-                break;
-            if (result % primes[i] == 0)
-            {
-                queue[counter++] = primes[i];
-                result /= primes[i];
-                i=-1;
-            }
+            fprintf(stderr, "Number is prime (-4)\n"); 
+            exit(-4);
+        }
+        u64* queue=get_prime_fact_ull(total, queue_size);
+        if (*queue == 0 || queue == NULL)
+        {
+            fprintf(stderr, "Error while parsing number (-6).\n");
+            exit(-6);
         }
         printf("{ ");
-        int acc = 1;
-        for(int i = 0; i<queue_size; i++) //Print the prime factors in a tidier way;
+        u64 acc = 1;
+        for (int i = 0; i<queue_size; i++) //Print the prime factors in a tidier way;
         {
-            if (queue[i]!=-99)
+            if (queue[i]!=0)
             {
                 if (queue[i]==queue[i-1])
                     acc++;
@@ -50,25 +46,31 @@ int main(int argc, char const *argv[])
                 {
                     if (i>0)
                         printf(" × ");
-                    printf("%d", queue[i]);
+                    printf("%llu", queue[i]);
                 }
                 if (queue[i]!=queue[i+1] && acc>1)
                 {
-                    printf("^%d", acc);
+                    printf("^%llu", acc);
                     acc = 1;
                 }
             }
             else
                 break;
         }
-        printf(" }\n");
+        printf(" } = ");
+        /* > Uncomment to enable further checking
+        acc = 1;
+        for (int i = 0; i < queue_size && queue[i] != 0; i++)
+            acc *= queue[i];
+        
+        printf("%llu == %llu\n", acc, input, total);
+        */
         free(queue);
-        free(primes);
         exit(0);
     }
     else
     {
-        fprintf(stderr, "No number provided (-4)\n");
-        exit(-4);
+        fprintf(stderr, "No number provided (-5)\n");
+        exit(-5);
     }
 }
